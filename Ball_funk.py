27e8614de356508395a8f828e681a752019c7820
RashoAli <43855304+RashoAli@users.noreply.github.com>
2018-10-17 03:59:10 +0200

    weiter feiner gemacht , keine golls waren festgelegt

--- Ball_funk.py
+++ Ball_funk.py
@@ -1,21 +1,27 @@
 import cv2
 import numpy as np
+import imutils
 
 
-class Ball_objekt():
+class Ball_Objekt():
+
     def __init__(self):
-        self.traverse_point = []
         self.total_rectangle = 9
         self.hand_rect_one_x = None
         self.hand_rect_one_y = None
 
         self.hand_rect_two_x = None
         self.hand_rect_two_y = None
-
+        '''wörde die farber schon calebriert'''
         self.Flag = None
+        '''' bestimung von position data'''
+        self.x_coordinate = 0
+        self.y_coordinate = 0
+        self.radius = 0
+        self.distance = 0
 
     def chek_x_y(self, frame):
-        ''' if the Boll on the bordder of the image Flag on '''
+        """ if the Boll on the bordder of the image Flag on """
         self.Flag = False
         rows, cols, _ = frame.shape
 
@@ -26,6 +32,7 @@ class Ball_objekt():
         return self.Flag
 
     def draw_rect(self, frame, is_ball_hist_created):
+        """draw wher the rect are """
         rows, cols, _ = frame.shape
         if not is_ball_hist_created:
             self.hand_rect_one_x = np.array(
@@ -44,13 +51,17 @@ class Ball_objekt():
         for i in range(self.total_rectangle):
             cv2.rectangle(frame, (self.hand_rect_one_y[i], self.hand_rect_one_x[i]),
                           (self.hand_rect_two_y[i], self.hand_rect_two_x[i]),
-                          (0, 255, 0), 1)
+                          (0, 0, 255), 1)
 
         return frame
 
-    def draw_moving_rect(self, frame, x, y, r):
+    def draw_moving_rect(self, frame):
+        """give the position where the rect should be drown"""
         rows, cols, _ = frame.shape
         li, re = 8, 2
+        x = self.x_coordinate
+        y = self.y_coordinate
+        r = self.radius
         self.hand_rect_one_x = np.array(
             [y - r / 2 - li, y - r / 2 - li, y - r / 2 - li, y - 5, y - 5, y - 5, y + r / 2 - re, y + r / 2 - re,
              y + r / 2 - re],
@@ -62,7 +73,7 @@ class Ball_objekt():
             dtype=np.uint32)
 
     def hand_histogram(self, frame):
-
+        """giv the max and min of the HSV """
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         roi = np.zeros([90, 10, 3], dtype=hsv_frame.dtype)
 
@@ -72,11 +83,33 @@ class Ball_objekt():
 
         data = np.zeros((3, 2))  # data = hsv min and max valus
 
-        data[0, 0] = np.median(roi[:, :, 0] - 20)
-        data[0, 1] = np.median(roi[:, :, 0] + 20)
-        data[1, 0] = np.median(roi[:, :, 1] - 50)
-        data[1, 1] = np.median(roi[:, :, 1] + 50)
+        data[0, 0] = np.median(roi[:, :, 0] - 50)
+        data[0, 1] = np.median(roi[:, :, 0] + 50)
+        data[1, 0] = np.median(roi[:, :, 1] - 60)
+        data[1, 1] = np.median(roi[:, :, 1] + 60)
         data[2, 0] = np.median(roi[:, :, 2] - 50)
         data[2, 1] = np.median(roi[:, :, 2] + 50)
-        # print(data)
         return data
+
+    '''contur funktion das schwirigste und langweiligste'''
+
+    def find_contur(self, mask):
+        """finding the ball"""
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+        center = None
+
+        # only proceed if at least one contour was for
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((self.x_coordinate, self.y_coordinate), self.radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        else:
+            center = np.zeros((2, 1))
+            self.radius = 0
+        return center, self.radius
--- balll.py
+++ balll.py
@@ -1,61 +1,32 @@
 # import the necessary packages
 from collections import deque
 from imutils.video import VideoStream
+import numpy as np
 import argparse
 import cv2
 import imutils
 import time
 
-from Ball_funk import Ball_objekt
+from Ball_funk import Ball_Objekt
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-                help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64,
-                help="max buffer size")
-args = vars(ap.parse_args())
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-pts = deque(maxlen=args["buffer"])
+'''' beim angfang kay : c = calibrating
+                        r = re_calibrating
+                        q = end             '''
 
-# if a video path was not supplied, grab the reference
-# to the webcam
-if not args.get("video", False):
-    vs = VideoStream(src=0).start()
 
-# otherwise, grab a reference to the video file
-else:
-    vs = cv2.VideoCapture(args["video"])
+vs = VideoStream(src=0).start()
 
-# allow the camera or video file to warm up
+# allow the camera or qqvideo file to warm up
 time.sleep(2.0)
-greenLower = (80, 40, 6)  # anfang werte fur die fabe
+greenLower = (40, 40, 6)  # anfang werte fur die fabe
 greenUpper = (120, 255, 255)
 # Define the codec and create VideoWriter object
 # deklerait ball_Objekt
-Ball_1 = Ball_objekt()
+Ball_1 = Ball_Objekt()
 is_ball_hist_created = False
 # keep looping
 while True:
     frame = vs.read()
-    if not is_ball_hist_created:
-        farme = Ball_1.draw_rect(frame, is_ball_hist_created)
-        hsv_data = Ball_1.hand_histogram(frame)
-        pressed_key = cv2.waitKey(1)
-        if pressed_key & 0xFF == ord('z'):
-            greenLower = (hsv_data[0, 0], hsv_data[1, 0], hsv_data[2, 0])
-            greenUpper = (hsv_data[0, 1], hsv_data[1, 1], hsv_data[2, 1])
-            is_ball_hist_created = True
-    ''' re_calibration fals gebraucht wird '''
-    re_calibration_key = cv2.waitKey(1)
-    if re_calibration_key & 0xFF == ord('r'):
-        is_ball_hist_created = False
-
-    # grab the current frame
-    # handle the frame from VideoCapture or VideoStream
-    frame = frame[1] if args.get("video", False) else frame
 
     # if we are viewing a video and we did not grab a frame,
     # then we have reached the end of the video
@@ -64,9 +35,38 @@ while True:
 
     # resize the frame, blur it, and convert it to the HSV
     # color space
-    frame = imutils.resize(frame, width=600)
+    #frame = imutils.resize(frame, width=600)
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    hsv[hsv[:, :, 0] < 50] = 0
+    hsv[hsv[:, :, 0] > 180] = 0
+
+    # check to calibrate the color borders
+    if not is_ball_hist_created:
+        farme = Ball_1.draw_rect(frame, is_ball_hist_created)
+        hsv_data = Ball_1.hand_histogram(frame)
+        pressed_key = cv2.waitKey(1)
+        if pressed_key & 0xFF == ord('c'):
+            greenLower = (hsv_data[0, 0], hsv_data[1, 0], hsv_data[2, 0])
+            greenUpper = (hsv_data[0, 1], hsv_data[1, 1], hsv_data[2, 1])
+            is_ball_hist_created = True
+
+    ''' re_calibration fals gebraucht wird '''
+    re_calibration_key = cv2.waitKey(1)
+    if re_calibration_key & 0xFF == ord('r'):
+        is_ball_hist_created = False
+
+
+    # update HSV range
+    if is_ball_hist_created:
+        Ball_1.draw_moving_rect(frame)
+        Flag = Ball_1.chek_x_y(frame)
+        if not Flag:
+            frame = Ball_1.draw_rect(frame, is_ball_hist_created)
+            hsv_data = Ball_1.hand_histogram(frame)
+            greenLower = (hsv_data[0, 0], hsv_data[1, 0], hsv_data[2, 0])
+            greenUpper = (hsv_data[0, 1], hsv_data[1, 1], hsv_data[2, 1])
+
 
     # construct a mask for the color "green", then perform
     # a series of dilations and erosions to remove any small
@@ -76,40 +76,13 @@ while True:
     mask = cv2.dilate(mask, None, iterations=2)
     # find contours in the mask and initialize the current
     # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-    center = None
-
-    # only proceed if at least one contour was for
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        # only proceed if the radius meets a minimum size
-
-        if radius > 10:
+    center,radius = Ball_1.find_contur(mask)
+    if radius > 10:
             # draw the circle and centroid on the frame,
             # then update the list of tracked points
-            cv2.circle(frame, (int(x), int(y)), int(radius),
+            cv2.circle(frame, (int(center[0]), int(center[1])), int(radius),
                        (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
-    # update the points queue
-    pts.appendleft(center)
-    # update HSV range
-
-    if is_ball_hist_created:
-        Ball_1.draw_moving_rect(frame, x, y, radius)
-        Flag = Ball_1.chek_x_y(frame)
-        if not Flag:
-            frame = Ball_1.draw_rect(frame, is_ball_hist_created)
-            hsv_data = Ball_1.hand_histogram(frame)
-            greenLower = (hsv_data[0, 0], hsv_data[1, 0], hsv_data[2, 0])
-            greenUpper = (hsv_data[0, 1], hsv_data[1, 1], hsv_data[2, 1])
 
     # loop over the set of tracked points
     # show the frame to our screen
@@ -122,9 +95,6 @@ while True:
     if key == ord("q"):
         break
 
-# if we are not using a video file, stop the camera video stream
-if not args.get("video", False):
-    vs.stop()
 
 # otherwise, release the camera
 else:
