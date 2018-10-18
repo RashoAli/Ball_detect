45bb6ef1bd163fd45b70bb672168831f9fe703e1
RashoAli <43855304+RashoAli@users.noreply.github.com>
2018-10-18 22:53:01 +0200

    Nach dem vorschlag von Danil

--- Ball_funk.py
+++ Ball_funk.py
@@ -4,146 +4,46 @@ import imutils
 from scipy import ndimage
 
 
-class Ball_Objekt():
+class ObjektBall:
+    """jeder ball Objekt"""
 
     def __init__(self):
-        self.total_rectangle = 9
-        self.hand_rect_one_x = None
-        self.hand_rect_one_y = None
-
-        self.hand_rect_two_x = None
-        self.hand_rect_two_y = None
-        '''wörde die farber schon calebriert'''
+        '''das bal würde schon einmal gesehen'''
         self.Flag = None
         '''' bestimung von position data'''
-        self.x_coordinate = 0
-        self.y_coordinate = 0
+        self.x_coordinate = 0  # x auf die coordienatensystem
+        self.y_coordinate = 0  # y auf die coordienatensystem
+        self.x_image = 0  # x auf dem image
+        self.y_image = 0  # y auf dem image
         self.radius = 0
         self.distance = 0
-
-    def chek_x_y(self, frame):
-        """ if the Boll on the bordder of the image Flag on """
-        self.Flag = False
-        rows, cols, _ = frame.shape
-
-        if any(self.hand_rect_one_x < 10) or any(self.hand_rect_one_y < 10) or any(
-                self.hand_rect_one_x > rows - 10) or any(
-            self.hand_rect_one_y > cols - 10):
-            self.Flag = True
-        return self.Flag
-
-    def draw_rect(self, frame, is_ball_hist_created):
-        """draw wher the rect are """
-        rows, cols, _ = frame.shape
-        if not is_ball_hist_created:
-            self.hand_rect_one_x = np.array(
-                [8 * rows / 20, 8 * rows / 20, 8 * rows / 20, 9 * rows / 20, 9 * rows / 20, 9 * rows / 20,
-                 10 * rows / 20,
-                 10 * rows / 20, 10 * rows / 20], dtype=np.uint32)
-
-            self.hand_rect_one_y = np.array(
-                [9 * cols / 25, 10 * cols / 25, 11 * cols / 25, 9 * cols / 25, 10 * cols / 25, 11 * cols / 25,
-                 9 * cols / 25,
-                 10 * cols / 25, 11 * cols / 25], dtype=np.uint32)
-
-        self.hand_rect_two_x = self.hand_rect_one_x + 10
-        self.hand_rect_two_y = self.hand_rect_one_y + 10
-
-        for i in range(self.total_rectangle):
-            cv2.rectangle(frame, (self.hand_rect_one_y[i], self.hand_rect_one_x[i]),
-                          (self.hand_rect_two_y[i], self.hand_rect_two_x[i]),
-                          (0, 0, 255), 1)
-
-        return frame
-
-    def draw_moving_rect(self, frame):
-        """give the position where the rect should be drown"""
-        rows, cols, _ = frame.shape
-        li, re = 8, 2
-        x = self.x_coordinate
-        y = self.y_coordinate
-        r = self.radius
-        self.hand_rect_one_x = np.array(
-            [y - r / 2 - li, y - r / 2 - li, y - r / 2 - li, y - 5, y - 5, y - 5, y + r / 2 - re, y + r / 2 - re,
-             y + r / 2 - re],
-            dtype=np.uint32)
-
-        self.hand_rect_one_y = np.array(
-            [x - r / 2 - li, x - 5, x + r / 2 - re, x - r / 2 - li, x - 5, x + r / 2 - re, x - r / 2 - li, x - 5,
-             x + r / 2 - re],
-            dtype=np.uint32)
-
-    def hand_histogram(self, frame):
-        """giv the max and min of the HSV """
-        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        roi = np.zeros([90, 10, 3], dtype=hsv_frame.dtype)
-
-        for i in range(self.total_rectangle):
-            roi[i * 10: i * 10 + 10, 0: 10] = hsv_frame[self.hand_rect_one_x[i]:self.hand_rect_one_x[i] + 10,
-                                              self.hand_rect_one_y[i]:self.hand_rect_one_y[i] + 10]
-
-        data = np.zeros((3, 2))  # data = hsv min and max valus
-
-        data[0, 0] = np.median(roi[:, :, 0] - 50)
-        data[0, 1] = np.median(roi[:, :, 0] + 50)
-        data[1, 0] = np.median(roi[:, :, 1] - 60)
-        data[1, 1] = np.median(roi[:, :, 1] + 60)
-        data[2, 0] = np.median(roi[:, :, 2] - 50)
-        data[2, 1] = np.median(roi[:, :, 2] + 50)
-        print(data)
-        return data
-
-    '''contur funktion das schwirigste und langweiligste'''
-
-    def find_contur(self, mask):
-        """finding the ball"""
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-        center = None
-
-        # only proceed if at least one contour was for
-        if len(cnts) > 0:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
-            c = max(cnts, key=cv2.contourArea)
-            ((self.x_coordinate, self.y_coordinate), self.radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            self.distance = 1 / self.radius
-        else:
-            center = np.zeros((2, 1))
-            self.radius = 0
-
-        return center, self.radius
 
 
 def mask_producer(frame, mask_HSV, color):
     mask = mask_HSV.copy()
     if color == 'Blau':
-        blueLower = (90, 40, 6)  # anfang werte fur die fabe
-        blueUpper = (120, 255, 255)
-        mask = cv2.inRange(mask, blueLower, blueUpper)
+        blue_lower = (90, 40, 6)  # anfang werte fur die fabe
+        blue_upper = (120, 255, 255)
+        mask = cv2.inRange(mask, blue_lower, blue_upper)
 
     if color == 'Grun':
-        greenLower = (55, 99, 80)  # anfang werte fur die fabe
-        greenUpper = (90, 200, 120)
-        mask = cv2.inRange(mask, greenLower, greenUpper)
+        green_lower = (40, 30, 10)  # anfang werte fur die fabe
+        green_upper = (80, 255, 255)
+        mask = cv2.inRange(mask, green_lower, green_upper)
         kernel = np.ones((20, 20), np.uint8)
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
-    mask = cv2.erode(mask, None, iterations=10)
-    mask = cv2.dilate(mask, None, iterations=10)
+    mask = cv2.erode(mask, None, iterations=5)
+    mask = cv2.dilate(mask, None, iterations=5)
 
     _, contours, _ = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
     centres = []
     for c in contours:
         # get the min area rect
-        ((x,y),r) = cv2.minEnclosingCircle(c)
+        ((x, y), r) = cv2.minEnclosingCircle(c)
         cv2.circle(frame, (int(x), int(y)), int(r),
                    (0, 255, 255), 2)
-        cv2.circle(frame, (int(x),int(y)), 5, (0, 0, 255), -1)
-        centres.append((int(x),int(y),int(r)))
+        cv2.circle(frame, (int(x), int(y)), 5, (0, 0, 255), -1)
+        centres.append((x, y, r))
 
-    return mask,centres
+    return mask, centres
--- balll.py
+++ balll.py
@@ -3,10 +3,10 @@ from imutils.video import VideoStream
 import cv2
 import time
 import matplotlib.pyplot as plt
-from Ball_funk import Ball_Objekt, mask_producer
+from Ball_funk import mask_producer,ObjektBall
 import numpy as np
 
-'''' beim angfang kay : c = calibrating
+'''' am Angfang kay : c = calibrating
                         r = re_calibrating
                         q = end             '''
 
@@ -35,7 +35,6 @@ greenLower = (40, 40, 6)  # anfang werte fur die fabe
 greenUpper = (120, 255, 255)
 # Define the codec and create VideoWriter object
 # deklerait ball_Objekt
-Ball_1 = Ball_Objekt()
 is_ball_hist_created = False
 # keep looping
 while True:
@@ -51,62 +50,18 @@ while True:
     if frame is None:
         break
 
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    # frame = imutils.resize(frame, width=600)
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    # plt.imshow(hsv)
-    # plt.show()
-    # mask_LAB = cv2.cvtColor(blurred, cv2.COLOR_BGR2LAB)
-    mask_HSV = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    median = cv2.medianBlur(frame, 5)
+    mask_HSV = cv2.cvtColor(median, cv2.COLOR_BGR2HSV)
+
     Blau_mask, Blau_c = mask_producer(frame, mask_HSV, 'Blau')
     Grun_mask, Grun_c = mask_producer(frame, mask_HSV, 'Grun')
 
     cv2.imshow('Blau_mask', Blau_mask)
     cv2.imshow('Grun_mask', Grun_mask)
-
-    # check to calibrate the color borders
-    if not is_ball_hist_created:
-        farme = Ball_1.draw_rect(frame, is_ball_hist_created)
-        hsv_data = Ball_1.hand_histogram(frame)
-        pressed_key = cv2.waitKey(1)
-        if pressed_key & 0xFF == ord('c'):
-            greenLower = (hsv_data[0, 0], hsv_data[1, 0], hsv_data[2, 0])
-            greenUpper = (hsv_data[0, 1], hsv_data[1, 1], hsv_data[2, 1])
-            is_ball_hist_created = True
-
-    ''' re_calibration fals gebraucht wird '''
-    re_calibration_key = cv2.waitKey(1)
-    if re_calibration_key & 0xFF == ord('r'):
-        is_ball_hist_created = False
-
-    # update HSV range
-    if is_ball_hist_created:
-        Ball_1.draw_moving_rect(frame)
-        Flag = Ball_1.chek_x_y(frame)
-        if not Flag:
-            frame = Ball_1.draw_rect(frame, is_ball_hist_created)
-            hsv_data = Ball_1.hand_histogram(frame)
-            greenLower = (hsv_data[0, 0], hsv_data[1, 0], hsv_data[2, 0])
-            greenUpper = (hsv_data[0, 1], hsv_data[1, 1], hsv_data[2, 1])
-
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    center, radius = Ball_1.find_contur(mask)
-
-
-    # loop over the set of tracked points
-    # show the frame to our screen
     cv2.imshow("Frame", frame)
     # print(radius)
-
+    #plt.imshow(mask_HSV)
+    #plt.show()
     key = cv2.waitKey(1) & 0xFF
 
     # if the 'q' key is pressed, stop the loop
